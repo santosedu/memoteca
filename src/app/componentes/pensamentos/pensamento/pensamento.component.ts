4a02564437c117ead20a98bd537b32bdf521ae3d
Eduardo Alves dos Santos <santosedu@live.com>
2023-10-24 22:01:33 -0300

    Acertando a arvore de componentes corretamente, interface Pensamento

--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Pensamento } from '../pensamento.ts';
 
 @Component({
   selector: 'app-pensamento',
@@ -6,7 +7,8 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./pensamento.component.css']
 })
 export class PensamentoComponent {
-  @Input() pensamento = {
+  @Input() pensamento : Pensamento {
+    id: 0,
     conteudo: 'Learning Angular, finally',
     autoria: 'Eduardo Alves dos Santos',
     modelo: 'modelo3'
