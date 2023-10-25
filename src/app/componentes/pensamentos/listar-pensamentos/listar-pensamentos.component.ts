b38195072dfef7e932c93cbe4e01c627b936464b
Eduardo Alves dos Santos <santosedu@live.com>
2023-10-25 10:53:43 -0300

    Criacao do arquivo de service

--- src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -7,24 +7,9 @@ import { Pensamento } from '../pensamento';
   styleUrls: ['./listar-pensamentos.component.css']
 })
 export class ListarPensamentosComponent {
-  listaPensamentos: Pensamento[] = [
-      {
-        id: 1,
-        conteudo: 'Minha função é passar informações para o componente filho',
-        autoria: 'Componente Pai',
-        modelo: 'modelo1'
-      },
-      {
-        id: 2,
-        conteudo: 'Minha propriedade é decorada com @Input()',
-        autoria: 'Componente filho',
-        modelo: 'modelo3'
-      },
-      {
-        id: 3,
-        conteudo: 'Estou testando um conteudo com mais de 256 caracteres. Esperto que realmente eu consiga fazer isso, já que não tenho a menor idéia do tamanho desse comentário. Na verdade estou achando bem mais simples e intuitivo aprender Angular come essa professora arretada, a Nay.',
-        autoria: 'Eduardo Santos',
-        modelo: 'modelo2'
-      },
-  ];
+  listaPensamentos: Pensamento[] = [];
+
+  constructor() {  }
+
+  ngOnInit(): void{}
 }
