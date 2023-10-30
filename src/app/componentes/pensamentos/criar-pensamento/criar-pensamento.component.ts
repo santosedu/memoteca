c754a5c62603b01efe4699a4ac1cc0239c52d90a
Eduardo Alves dos Santos <santosedu@live.com>
2023-10-30 12:09:31 -0300

    Correcao com a inclusao do ngModel no componente de criar pensamento

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class CriarPensamentoComponent implements OnInit{
   pensamento : Pensamento = {
-    conteudo: ''
+    conteudo: '',
     autoria: '',
     modelo: ''
   }
